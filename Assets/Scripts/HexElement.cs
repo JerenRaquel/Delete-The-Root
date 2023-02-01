ea77703477272266558f3ceb41b17ddc7bd970f4
Jeren Raquel <jeraquel101@gmail.com>
2023-02-01 10:45:55 -0800

    Update HexElement.cs

--- Assets/Scripts/HexElement.cs
+++ Assets/Scripts/HexElement.cs
@@ -6,13 +6,20 @@ public class HexElement : MonoBehaviour {
     public string HexStr { get; private set; }
 
     private static char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F' };
+    private TMPro.TextMeshPro textBox;
+
+    private void Awake() {
+        this.textBox = gameObject.GetComponent<TMPro.TextMeshPro>();
+    }
 
     public void Initialize(string forcedHexStr = null) {
         if (forcedHexStr == null) {
             int num = Random.Range(1, 10);
             char letter = letters[Random.Range(0, letters.Length)];
+            this.HexStr = num.ToString() + letter;
         } else {
             this.HexStr = forcedHexStr;
         }
+        this.textBox.text = this.HexStr;
     }
 }
